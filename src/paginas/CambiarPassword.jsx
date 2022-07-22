064e0b8a0f27bc59b302c70863d6db1b976ba426
Alejandro Ramirez <alex@iMac-de-Alejandro.local>
2022-07-22 17:15:38 -0600

    quitar enlace de cambiar contraseña de la pagina cambiar-password

--- src/paginas/CambiarPassword.jsx
+++ src/paginas/CambiarPassword.jsx
@@ -48,7 +48,7 @@ const CambiarPassword = () => {
   const {msg} = alerta
   return (
     <>
-        <AdminNav />
+        
         <h2 className="font-black text-3xl text-center mt-10">Cambiar contraseña</h2>
         <p className="text-xl mt-5 mb-10 text-center">Modifica tu contraseña</p>
         <div className=" flex justify-center">
